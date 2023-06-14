26f43c061a698206183ac7a7a8973d6c2647a7fc
el-kg <kirillgalkin8@gmail.com>
2023-06-14 18:01:40 +0700

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Storage storage = new Storage();
+        storage.addEmployee ("Иванов Иван Иванович ",1,20000);
+        storage.addEmployee("Петров Петр Петрович ",1,22000);
+        storage.addEmployee("Фудоров Федор Федорович ",2,22000);
+        storage.addEmployee("Алексеев Алексей Алексеевич ",2,24000);
+        storage.addEmployee("Александров Александр Александрович ",3,24000);
+        storage.addEmployee("Николаев Николай Николаевич ",3,26000);
+        storage.addEmployee("Родионов Родион Родионович ",4,26000);
+        storage.addEmployee("Сергеев Сергей Сергеевич ",4,28000);
+        storage.addEmployee("Дмитриев Дмитрий Дмитриевич ",5,28000);
+        storage.addEmployee("Павлов Павел Павлович ",5,30000);
+
+        storage.printAllEmployee();
+        storage.costAmount();
+        storage.minSalary();
+        storage.maxSalary();
+        storage.AverageSalary();
+        storage.printAllEmployeeName();
+
+
+    }
+}
---
+++ src/Storage.java
@@ -0,0 +1,62 @@
+public class Storage {
+        private final Employee[] employees = new Employee[10];
+        private int size;
+    public void addEmployee (String name, int department, int salary) {
+        if (size >= employees.length) {
+            System.out.println("Нельзя добавить сотрудника, в хранилище закончилось место");
+        }
+        Employee newEmployee = new Employee(name,department,salary);
+        employees[size++] = newEmployee;
+    }
+    public int getCurrentSize() {
+        return size;
+    }
+    public void printAllEmployee() {
+        for (int i = 0; i < size; i++) {
+            Employee employee = employees[i];
+            System.out.println(employee.toString());
+        }
+    }
+    public void printAllEmployeeName() {
+        for (int i = 0; i < size; i++) {
+            Employee employee = employees[i];
+            System.out.println(employee.getName());
+        }
+    }
+    public void costAmount(){
+        int costAmount = 0;
+        for (int i = 0; i < size; i++) {
+            Employee employee = employees[i];
+            costAmount += employee.getSalary();
+        }
+        System.out.println("Сумма выплат на ЗП в месяц составляет : "+ costAmount);
+    }
+    public void minSalary(){
+        int minCost =30_000;
+        for (int i = 0; i < size; i++){
+            Employee employee = employees[i];
+            if (employee.getSalary() < minCost){
+                minCost = employee.getSalary();
+            }
+        }
+        System.out.println("Минимальная зарплата составляет : " + minCost);
+    }
+    public void maxSalary (){
+        int maxCost =0;
+        for (int i = 0; i < size; i++){
+            Employee employee = employees[i];
+            if (employee.getSalary() >maxCost){
+                maxCost = employee.getSalary();
+            }
+        }
+        System.out.println("Максимальная зарплата составляет : " +maxCost);
+    }
+    public void AverageSalary(){
+        int costAmount= 0;
+        for (int i = 0; i < size; i++){
+            Employee employee = employees[i];
+            costAmount+= employee.getSalary();
+        }
+        System.out.println("Средняя ЗП : "+ costAmount/size);
+    }
+}
